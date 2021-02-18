20e704093d6a69b04e3f67e5c0dffd2aeedcb2ad
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-02-18 16:14:22 +0200

    setup files and the server

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static('./public'));
+
 app.listen(PORT, () => {
 	console.log(`server on localhost:${PORT}`);
 });
